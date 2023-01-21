d3aa9964b57aa872147be6240180da4ac045ca63
Christoforos Vradis <chrisvradis00@gmail.com>
2023-01-22 00:12:02 +0200

    feat(page): add 404 page and cosmetics

---
+++ error.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <p class="title">404</p>
+    <p class="message">{{ message }}</p>
+    <NuxtLink :to="{ path: '/' }">
+      <button class="goHomeButton">Home</button>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref(
+  "Oops! Looks like you took a wrong turn. But don't worry, there's always a way back home."
+);
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.title {
+  font-size: 100px;
+  animation: pulse 2s infinite;
+}
+
+.message {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 1rem;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,13 @@
 <template>
   <div class="home">
     <h2>&#129302; FTS QuizBot &#129302;</h2>
-    <p>Click on a category to select it</p>
+    <p>Click/Tap on a category to select it</p>
+    <p style="margin-top: 0px">Swiping doesn't work &#128531;</p>
     <CategoriesContainer />
   </div>
 </template>
 
-<style>
+<style scoped>
 @font-face {
   font-family: "Raleway";
   src: url("./../assets/fonts/Raleway-Regular.ttf") format("truetype");
@@ -27,5 +28,9 @@ body {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+p:nth-child(n + 3) {
+  text-align: center;
+  margin-top: 0;
 }
 </style>
